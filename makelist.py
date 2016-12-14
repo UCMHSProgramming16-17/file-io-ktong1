7634e1b86de2deaaf9de352f302086ba94692f17
Kevin Tong <ktong@ucvts.tec.nj.us>
2016-12-14 14:29:01 +0000

    changed makelist

--- makelist.py
+++ makelist.py
@@ -2,6 +2,6 @@
 file = open('list_1.txt', 'w')
 # write number (1-10) and the user's input, then a new line
 for x in range(10):
-    file.write( str(x+1) '. '+ input(str(x+1) + '. ') + '\n')
+    file.write( str(x+1) + '. '+ input(str(x+1) + '. ') + '\n')
 # close file
 file.close()
