5f351fa931e52b5478d95a23ec91519c98d4b9e9
Kevin Tong <ktong@ucvts.tec.nj.us>
2017-01-01 20:20:02 +0000

    data project

---
+++ data_project/csvdata.py
@@ -0,0 +1,10 @@
+# import needed modules
+import csv
+import math
+import 
+
+# Question: Compare the production of 4 different energy sources (renewable, nuclear, natrual gas, petroleum) in kJs
+
+def collectdata():
+    
+    EIA_APIkey = "a0758428314605c2172e496804668b98"
