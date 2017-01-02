54f63a208ec9700f57ef0b3507581f44707ed557
Kevin Tong <ktong@ucvts.tec.nj.us>
2017-01-02 01:40:24 +0000

    new functions

--- data_project/csvdata.py
+++ data_project/csvdata.py
@@ -5,36 +5,87 @@ import requests
 
 # Question: Compare the production of 4 different energy sources (renewable, nuclear, natrual gas, crude oil) in kJs, by month, over the last 20 years
 
-# Collect necessary data and store data in a csv file
-def collectdata():
-    # define api basics
-    # Note: url, with various ids and series, found on eia site.
-    EIA_APIkey = "a0758428314605c2172e496804668b98"
+# Define global EIA API key variable
+EIA_APIkey = "a0758428314605c2172e496804668b98"
+ 
+# Collect renewable data
+def collectdata_renewable():
+    # Define api basics
+    # Note: url, with various ids and series, found on eia site
     url_renewable = "http://api.eia.gov/series/?api_key=YOUR_API_KEY_HERE&series_id=TOTAL.REPRBUS.M"
-    url_nuclear = "http://api.eia.gov/series/?api_key=YOUR_API_KEY_HERE&series_id=TOTAL.NUETPUS.M"
-    url_naturalgas = "http://api.eia.gov/series/?api_key=YOUR_API_KEY_HERE&series_id=TOTAL.NGELPUS.M"
-    url_oil = "http://api.eia.gov/series/?api_key=YOUR_API_KEY_HERE&series_id=TOTAL.PAPRPUS.M"
     
     # Insert EIA API key into url
     url_renewable = url_renewable.replace("YOUR_API_KEY_HERE", EIA_APIkey)
-    url_nuclear = url_nuclear.replace("YOUR_API_KEY_HERE", EIA_APIkey)
-    url_naturalgas = url_naturalgas.replace("YOUR_API_KEY_HERE", EIA_APIkey)
-    url_oil = url_oil.replace("YOUR_API_KEY_HERE", EIA_APIkey)
     
     # Send data requests and store in a variable
     json_renewable = requests.get(url_renewable)
-    json_nuclear = requests.get(url_nuclear)
-    json_naturalgas = requests.get(url_naturalgas)
-    json_oil = requests.get(url_oil)
     
     # Convert from json into python
     renewable_data = json_renewable.json()
+    
+    # return data
+    return renewable_data
+
+# Collect nuclear data
+def collectdata_nuclear():
+    # Define api basics
+    # Note: url, with various ids and series, found on eia site.
+    url_nuclear = "http://api.eia.gov/series/?api_key=YOUR_API_KEY_HERE&series_id=TOTAL.NUETPUS.M"
+   
+    # Insert EIA API key into url
+    url_nuclear = url_nuclear.replace("YOUR_API_KEY_HERE", EIA_APIkey)
+    
+    # Send data requests and store in a variable
+    json_nuclear = requests.get(url_nuclear)
+    
+    # Convert from json into python
     nuclear_data = json_nuclear.json()
+    
+    # return data
+    return nuclear_data
+
+# Collect natural gas data
+def collectdata_naturalgas():
+    # Define api basics
+    # Note: url, with various ids and series, found on eia site
+    url_naturalgas = "http://api.eia.gov/series/?api_key=YOUR_API_KEY_HERE&series_id=TOTAL.NGELPUS.M"
+    
+    # Insert EIA API key into url
+    url_naturalgas = url_naturalgas.replace("YOUR_API_KEY_HERE", EIA_APIkey)
+    
+    # Send data requests and store in a variable
+    json_naturalgas = requests.get(url_naturalgas)
+    
+    # Convert from json into python
     naturalgas_data = json_naturalgas.json()
+    
+    # return data
+    return naturalgas_data
+
+# Collect oil data
+def collectdata_oil():
+    # Define api basics
+    # Note: url, with various ids and series, found on eia site
+    url_oil = "http://api.eia.gov/series/?api_key=YOUR_API_KEY_HERE&series_id=TOTAL.PAPRPUS.M"
+    
+    # Insert EIA API key into url
+    url_oil = url_oil.replace("YOUR_API_KEY_HERE", EIA_APIkey)
+    
+    # Send data requests and store in a variable
+    json_oil = requests.get(url_oil)
+    
+    # Convert from json into python
     oil_data = json_oil.json()
     
+    # return data
+    return oil_data
+
+
+# store data in csv files  
+def storedata():
+    # make csv files
+    open 
     # Put data into csv files
     
-    print (oil_data)
 
 collectdata()
