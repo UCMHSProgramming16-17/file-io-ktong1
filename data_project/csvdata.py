b469403160643026320d7ba2acf522e00e34cafd
Kevin Tong <ktong@ucvts.tec.nj.us>
2017-01-02 04:04:50 +0000

    added store data and conversions

--- data_project/csvdata.py
+++ data_project/csvdata.py
@@ -3,7 +3,7 @@ import csv
 import math
 import requests
 
-# Question: Compare the production of 4 different energy sources (renewable, nuclear, natrual gas, crude oil) in kJs, by month, over the last 20 years
+# Question: Compare the production of 3 different energy sources (renewable, nuclear, natrual gas) in kJs, by month, over the last 20 years
 
 # Define global EIA API key variable
 EIA_APIkey = "a0758428314605c2172e496804668b98"
@@ -22,8 +22,13 @@ def collectdata_renewable():
     json_renewable = requests.get(url_renewable)
     
     # Convert from json into python
-    renewable_data = json_renewable.json()
+    renewable_0 = json_renewable.json()
     
+    # Convert data into kJ from trillions of butanes (1055055852620 kJ per trillion butane)
+    renewable_data = renewable_0['series'][0]['data']
+    for a in renewable_data:
+        a[1] = a[1]*1055055852620
+        
     # Return data
     return renewable_data
 
@@ -41,8 +46,13 @@ def collectdata_nuclear():
     json_nuclear = requests.get(url_nuclear)
     
     # Convert from json into python
-    nuclear_data = json_nuclear.json()
+    nuclear_0 = json_nuclear.json()
     
+    # Convert data from millions of kilowatt-hours into kJ (3600000000kJ per million of kilowatt-hours)
+    nuclear_data = nuclear_0['series'][0]['data']
+    for a in nuclear_data:
+        a[1] = a[1]*3600000000
+        
     # Return data
     return nuclear_data
 
@@ -60,33 +70,18 @@ def collectdata_naturalgas():
     json_naturalgas = requests.get(url_naturalgas)
     
     # Convert from json into python
-    naturalgas_data = json_naturalgas.json()
+    naturalgas_0 = json_naturalgas.json()
     
+    # Convert data from billions of cubic feet of natural gas to kJ (1055055852620kJ per billion ft^3 of natural gas)
+    naturalgas_data = naturalgas_0['series'][0]['data']
+    for a in naturalgas_data:
+        a[1] = a[1]*1055055852620
+   
     # Return data
     return naturalgas_data
 
-# Collect oil data
-def collectdata_oil():
-    
-    # Define api basics
-    # Note: url, with various ids and series, found on eia site
-    url_oil = "http://api.eia.gov/series/?api_key=YOUR_API_KEY_HERE&series_id=TOTAL.PAPRPUS.M"
-    
-    # Insert EIA API key into url
-    url_oil = url_oil.replace("YOUR_API_KEY_HERE", EIA_APIkey)
-    
-    # Send data requests and store in a variable
-    json_oil = requests.get(url_oil)
-    
-    # Convert from json into python
-    oil_data = json_oil.json()
-    
-    # Return data
-    return oil_data
-
-
 # Store data in csv files  
-def storedata(data, csvtitle):
+def storedata(data1, data2, data3, type1, type2, type3, csvtitle):
     
     # Make csv files
     csvfile = open(csvtitle, "w")
@@ -95,4 +90,9 @@ def storedata(data, csvtitle):
     csvwriter = csv.writer(csvfile, delimiter = ",")
     
     # Put data into csv files
-    
+    csvwriter.writerow(['Year', 'Month', type1 + ' (kJ)', type2 + ' (kJ)', type3 + ' (kJ)'])
+    for a in data1:
+        csvwriter.writerow(a[0][0:3], a[0][4:5], a[1], data2[a][1], data3[a][1])
+
+# Call all functions
+storedata(collectdata_renewable(), collectdata_nuclear(), collectdata_naturalgas(), 'Renewable Energy', 'Nuclear Energy', 'Natural Gas Energy', 'energydata.csv')
